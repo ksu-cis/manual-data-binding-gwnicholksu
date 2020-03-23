aec949447372c5fa48143915b092ec73630d6ed8
Grant Nichol <gwnichol@ksu.edu>
2020-03-23 11:59:16 -0500

    Finish Lecture Participation

--- ManualDataBinding/MainWindow.xaml.cs
+++ ManualDataBinding/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ManualDataBinding.Data;
 
 namespace ManualDataBinding
 {
@@ -20,9 +21,53 @@ namespace ManualDataBinding
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Create the note to add to our editor
+        Note note = new Note();
+
         public MainWindow()
         {
             InitializeComponent();
+            Editor.Note = note;
+        }
+
+        /// <summary>
+        /// Evenbt handler tocreate a new note and apply it to the editor
+        /// </summary>
+        /// <param name="sender">The button clicked</param>
+        /// <param name="e">the event args</param>
+        public void OnNewNote(object sender, RoutedEventArgs e)
+        {
+            note = new Note();
+            Editor.Note = note;
+        }
+
+        /// <summary>
+        /// Evenbt handler to clear the contents of the note
+        /// </summary>
+        /// <param name="sender">The button clicked</param>
+        /// <param name="e">the event args</param>
+        public void OnClearNote(object sender, RoutedEventArgs e)
+        {
+            note.Body = "";
+        }
+
+        /// <summary>
+        /// Event Handler to mutate the current note
+        /// </summary>
+        /// <param name="sender">The button clicked</param>
+        /// <param name="e">The event args</param>
+        public void OnMutateNote(object sender, RoutedEventArgs e)
+        {
+            note.Title = "Darth Plagueis The Wise";
+            note.Body = "Did you ever hear the tragedy of Darth Plagueis The Wise? " +
+                        "I thought not. It's not a story the Jedi would tell you. It's a Sith legend. " +
+                        "Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use " +
+                        "the Force to influence the midichlorians to create life… He had such a knowledge " +
+                        "of the dark side that he could even keep the ones he cared about from dying. " +
+                        "The dark side of the Force is a pathway to many abilities some consider to be unnatural. " +
+                        "He became so powerful… the only thing he was afraid of was losing his power, which eventually, " +
+                        "of course, he did. Unfortunately, he taught his apprentice everything he knew, " +
+                        "then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.";
         }
     }
 }
--- ManualDataBinding/UI/NoteEditor.xaml.cs
+++ ManualDataBinding/UI/NoteEditor.xaml.cs
@@ -19,9 +19,60 @@ namespace ManualDataBinding.UI
     /// </summary>
     public partial class NoteEditor : UserControl
     {
+        private Note note;
+        /// <summary>
+        /// The Note that will be edited by this control
+        /// </summary>
+        public Note Note {
+            get { return note; }
+            set
+            {
+                if (note != null) note.NoteChanged -= OnNoteChange;
+                note = value;
+                if (note != null)
+                {
+                    note.NoteChanged += OnNoteChange;
+                    OnNoteChange(this, new EventArgs());
+                }
+            }
+        }
+
         public NoteEditor()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Listener for changes in the note property
+        /// </summary>
+        /// <param name="sender">The sending object</param>
+        /// <param name="e">Event args</param>
+        public void OnNoteChange(object sender, EventArgs e)
+        {
+            if (Note == null) return;
+
+            Title.Text = Note.Title;
+            Body.Text = Note.Body;
+        }
+
+        /// <summary>
+        /// Run when title has changed
+        /// </summary>
+        /// <param name="sender">The sending object</param>
+        /// <param name="e">Event args</param>
+        public void OnTitleChanged(object sender, TextChangedEventArgs e)
+        {
+            Note.Title = Title.Text;
+        }
+
+        /// <summary>
+        /// Run when the body has changed
+        /// </summary>
+        /// <param name="sender">The sending object</param>
+        /// <param name="e">Event Args</param>
+        public void OnBodyChanged(object sender, TextChangedEventArgs e)
+        {
+            Note.Body = Body.Text;
         }
     }
 }
